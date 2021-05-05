6a311318e90f03a78ed66f2ef5eb5ace6c417355
nurdann <nurdann@localhost>
2021-05-04 22:26:11 -0700

    Added simple web interface

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let input = document.getElementById("input");
+    let outputElement = document.getElementById("evaluation-history");
+
+    let form = document.getElementById("form");
+    let evalButton = document.getElementById("evaluate");
+    
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        evalText(input.value, outputElement);
+    });
+
+    evalButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        evalText(input.value, outputElement);
+    })
+
+});
+
+function evalText(text, outputElement) {
+        let result = evalPostfix(ShuntingYard(text));
+        appendResult(outputElement, text + " = " + result);
+}
+
+function appendResult(outputElement, outputText) {
+    let li = document.createElement("li");
+    let textElement = document.createTextNode(outputText);
+    li.appendChild(textElement);
+    outputElement.appendChild(li);
+}
